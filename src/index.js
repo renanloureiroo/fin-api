a6fd458b3e2b1e64a4ff8134b865aa9250035626
Renan Pereira Loureiro <renanpl47@gmail.com>
2021-11-03 15:39:15 -0300

    saque

--- src/index.js
+++ src/index.js
@@ -24,6 +24,18 @@ const verifyIfExistsAccountCPF = (request, response, next) => {
   return next()
 
 }
+
+const getBalance = (statement) => {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === "credit") {
+      return acc + operation.amount
+    } else {
+      return acc - operation.amount
+    }
+  }, 0)
+
+  return balance
+}
 // create account
 app.post('/account', (request, response) => {
 
@@ -72,6 +84,32 @@ app.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
   return response.status(201).send()
 })
 
+
+// saque
+app.post('/withdraw', verifyIfExistsAccountCPF, (request, response) => {
+  const { amount } = request.body
+  const { customer } = request
+
+  const balance = getBalance(customer.statement)
+
+  if (balance <= amount) {
+    return response.status(400).json({
+      error: 'Insufficient funds!'
+    })
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: 'debit'
+  }
+
+  customer.statement.push(statementOperation)
+
+  return response.status(201).send()
+
+
+})
 
 // get extrato
 app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
